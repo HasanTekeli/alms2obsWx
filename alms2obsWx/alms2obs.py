7964821f9167268c9e193ca26307edb6a1b93a00
Hasan A. Tekeli <hasan_tekeli@yahoo.com>
2021-06-02 10:07:28 +0300

    pref window updated2

--- alms2obsWx/alms2obs.py
+++ alms2obsWx/alms2obs.py
@@ -2,10 +2,11 @@ import glob
 import os
 import wx
 import wx.lib.mixins.listctrl as listmix
+import json
 from funcs import create_data, organize_results
 
 
-class resizedListCtrl(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
+class ResizedListCtrl(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
     def __init__(self, parent, ID, pos=wx.DefaultPosition,
                  size=wx.DefaultSize, style=0):
         wx.ListCtrl.__init__(self, parent, ID, pos, size, style)
@@ -18,7 +19,7 @@ class MyPanel(wx.Panel):
         tID = wx.ID_ANY
         dayTxt = wx.StaticText(self, -1, label="GÜNDÜZ", style=wx.ALIGN_CENTER)
         nightTxt = wx.StaticText(self, -1, label="AKŞAM", style=wx.ALIGN_CENTER)
-        self.list_ctrl = resizedListCtrl(self, tID,
+        self.list_ctrl = ResizedListCtrl(self, tID,
                                          style=wx.LC_REPORT
                                          | wx.BORDER_SUNKEN
                                          )
@@ -26,7 +27,7 @@ class MyPanel(wx.Panel):
         self.list_ctrl.InsertColumn(1, 'Durum')
         self.list_ctrl.SetColumnWidth(0, 400)
 
-        self.list_ctrlNight = resizedListCtrl(self, tID,
+        self.list_ctrlNight = ResizedListCtrl(self, tID,
                                               style=wx.LC_REPORT
                                               | wx.BORDER_SUNKEN
                                               )
@@ -37,34 +38,46 @@ class MyPanel(wx.Panel):
         btn = wx.Button(self, label="Klasör seç")
         btn.Bind(wx.EVT_BUTTON, self.onOpenDirectory)
 
+        pref_btn = wx.Button(self, wx.ID_ANY, label="Tercihler")
+        pref_btn.Bind(wx.EVT_BUTTON, self.openPrefs)
+
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(dayTxt, 0, wx.EXPAND, 5)
         sizer.Add(self.list_ctrl, 1, wx.ALL|wx.EXPAND, 5)
         sizer.Add(nightTxt, 0, wx.EXPAND, 5)
         sizer.Add(self.list_ctrlNight, 1, wx.ALL | wx.EXPAND, 5)
         sizer.Add(btn, 0, wx.ALL|wx.CENTER, 5)
+        sizer.Add(pref_btn, 0, wx.ALL | wx.CENTER, 5)
         self.SetSizer(sizer)
 
         self.folder_path = ""
         self.list_of_deps = {}
 
+    def openPrefs(self, event):
+        dlg = PreferencesDialog()
+        dlg.ShowModal()
+
     def onOpenDirectory(self, event):
         dlg = wx.DirDialog(self, "Choose a directory:")
         if dlg.ShowModal() == wx.ID_OK:
             self.folder_path = dlg.GetPath()
-            self.updateDisplay(organize_results(self.folder_path, self.list_of_deps))
+            self.updateDisplay(organize_results(self.folder_path, self.list_of_deps, current_dir))
         dlg.Destroy()
 
     def updateDisplay(self, mods_path):
-        dayPath = glob.glob(mods_path + "day/*.*")
-        nightPath = glob.glob(mods_path + "night/*.*")
-        for index, path in enumerate(dayPath):
-            self.list_ctrl.InsertItem(index, os.path.basename(path))
-        self.list_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onClickListItem, self.list_ctrl)
-        for index, path in enumerate(nightPath):
-            self.list_ctrlNight.InsertItem(index, os.path.basename(path))
-        self.list_ctrlNight.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onClickListItem, self.list_ctrlNight)
-
+        try:
+            dayPath = glob.glob(mods_path + "day/*.*")
+            nightPath = glob.glob(mods_path + "night/*.*")
+            for index, path in enumerate(dayPath):
+                self.list_ctrl.InsertItem(index, os.path.basename(path))
+            self.list_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onClickListItem, self.list_ctrl)
+            for index, path in enumerate(nightPath):
+                self.list_ctrlNight.InsertItem(index, os.path.basename(path))
+            self.list_ctrlNight.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onClickListItem, self.list_ctrlNight)
+        except TypeError:
+            error_dlg = wx.MessageDialog(self, "Dosyalarınızdan birinde öğrenci numarası metin şeklinde yazılmış!\nLütfen düzeltip tekrar deneyin.", caption="Hata!")
+            error_dlg.ShowModal()
+            error_dlg.Destroy()
     def onClickListItem(self, event):
         dayPath = self.folder_path + "/mods/day/"
         nightPath = self.folder_path + "/mods/night/"
@@ -96,7 +109,212 @@ class MyFrame(wx.Frame):
         self.Show()
 
 
+class PreferencesDialog(wx.Dialog):
+    """
+    Creates and displays a preferences dialog that allows the user to
+    change some settings.
+    """
+
+    # ----------------------------------------------------------------------
+    def __init__(self):
+        """
+        Initialize the dialog
+        """
+        wx.Dialog.__init__(self, None, wx.ID_ANY, 'Preferences', size=(550, 300))
+        self.cfg_file = current_dir + "/settings.json"
+        with open(self.cfg_file, "r") as config:
+            self.data = json.load(config)
+        self.createWidgets()
+
+    # ----------------------------------------------------------------------
+    def createWidgets(self):
+        """
+        Create and layout the widgets in the dialog
+        """
+        lblSizer = wx.BoxSizer(wx.VERTICAL)
+        valueSizer = wx.BoxSizer(wx.VERTICAL)
+        btnSizer = wx.StdDialogButtonSizer()
+        colSizer = wx.BoxSizer(wx.HORIZONTAL)
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.widgetNames = self.data
+        font = wx.Font(12, wx.SWISS, wx.NORMAL, wx.BOLD)
+
+        num_lbl = wx.StaticText(self, label="Numaraların olduğu hücre sütunu:")
+        num_lbl.SetFont(font)
+        grade_lbl = wx.StaticText(self, label="Notların olduğu hücre sütunu:")
+        grade_lbl.SetFont(font)
+        dep_lbl = wx.StaticText(self, label="Bölümün/Dersin yazdığı bir hücre (Örn. C2):")
+        dep_lbl.SetFont(font)
+        lblSizer.Add(num_lbl, 0, wx.ALL, 10)
+        lblSizer.Add(grade_lbl, 0, wx.ALL, 10)
+        lblSizer.Add(dep_lbl, 0, wx.ALL, 10)
+        num_value = self.data["num_col"]
+        grade_value = self.data["grade_col"]
+        dep_value = self.data["dep_cell"]
+        num_txt = wx.TextCtrl(self, value=num_value, name="num_col")
+        grade_txt = wx.TextCtrl(self, value=grade_value, name="grade_col")
+        dep_txt = wx.TextCtrl(self, value=dep_value, name="dep_cell")
+        valueSizer.Add(num_txt, 0, wx.ALL | wx.EXPAND, 5)
+        valueSizer.Add(grade_txt, 0, wx.ALL | wx.EXPAND, 5)
+        valueSizer.Add(dep_txt, 0, wx.ALL | wx.EXPAND, 5)
+
+        saveBtn = wx.Button(self, wx.ID_OK, label="Save")
+        saveBtn.Bind(wx.EVT_BUTTON, self.onSave)
+        btnSizer.AddButton(saveBtn)
+
+        cancelBtn = wx.Button(self, wx.ID_CANCEL)
+        btnSizer.AddButton(cancelBtn)
+
+        depButton = wx.Button(self, wx.ID_OPEN, label="Departments")
+        depButton.Bind(wx.EVT_BUTTON, self.openDepList)
+        lblSizer.AddSpacer(10)
+        lblSizer.Add(depButton)
+        btnSizer.Realize()
+
+        colSizer.Add(lblSizer)
+        colSizer.Add(valueSizer, 1, wx.EXPAND)
+        mainSizer.Add(colSizer, 0, wx.EXPAND)
+        mainSizer.Add(btnSizer, 0, wx.ALL | wx.ALIGN_RIGHT, 5)
+        self.SetSizer(mainSizer)
+
+    # ----------------------------------------------------------------------
+    def onSave(self, event):
+        """
+        Saves values to disk
+        """
+        for name in self.widgetNames:
+            widget = wx.FindWindowByName(name)
+            if isinstance(widget, wx.TextCtrl):
+                value = widget.GetValue()
+                self.widgetNames[name] = value
+            else:
+                pass
+
+        with open(self.cfg_file, "w") as config:
+            json.dump(self.data, config, ensure_ascii=False, indent=4)
+        self.EndModal(0)
+
+    def openDepList(self, event):
+        dlg = DepartmentsDialog()
+        dlg.ShowModal()
+
+
+class DepartmentsDialog(wx.Dialog):
+    """
+    Creates and displays a dialog where departments are listed.
+    """
+    def __init__(self):
+        wx.Dialog.__init__(self, None, wx.ID_ANY, 'Departments', size=(400, 700))
+
+        self.cfg_file = "settings.json"
+        with open(self.cfg_file, "r") as config:
+            self.data = json.load(config)
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        txt = 'Bölüm Arayın. Büyük-küçük "İ-i" harfine dikkat edin.'
+        label = wx.StaticText(self, label=txt)
+
+        self.main_sizer.Add(label, 0, wx.ALL, 5)
+        self.search = wx.SearchCtrl(
+            self, style=wx.TE_PROCESS_ENTER, size=(-1, 25))
+        self.search.Bind(wx.EVT_SEARCHCTRL_SEARCH_BTN, self.on_search)
+        self.search.Bind(wx.EVT_TEXT_ENTER, self.on_search)
+
+        self.main_sizer.Add(self.search, 0, wx.EXPAND)
+
+        self.choices = self.data["departments"]
+
+        self.dep_list_lbo = wx.ListBox(self,
+                                      size=wx.DefaultSize, choices=self.choices,
+                                      style=wx.LB_NEEDED_SB | wx.CB_READONLY,
+                                      )
+        self.dep_list_lbo.Set(self.choices)
+        self.search_results_lbo = wx.ListBox(self,
+                                      size=(10,-1),
+                                      style=wx.LB_NEEDED_SB | wx.CB_READONLY,
+                                      )
+
+        txt_src_res = "Search Results"
+        lbl_src_res = wx.StaticText(self, label=txt_src_res)
+        self.main_sizer.Add(lbl_src_res, 0, wx.EXPAND)
+        self.main_sizer.Add(self.search_results_lbo, 2, wx.EXPAND)
+        txt_all_deps = "All Departments"
+        lbl_all_deps = wx.StaticText(self, label=txt_all_deps)
+        add_dep_btn = wx.Button(self, wx.ID_ANY, label="Yeni Bölüm / Ders Ekleyin")
+        add_dep_btn.Bind(wx.EVT_BUTTON, self.add_dep)
+
+        refresh_txt = "Yeni eklediğiniz bölümlerin görünmesi için listeyi yenileyin:"
+        refresh_lbl = wx.StaticText(self, label=refresh_txt)
+        refresh_btn = wx.Button(self, wx.ID_ANY, label="Listeyi yenile")
+        refresh_btn.Bind(wx.EVT_BUTTON, self.refresh_deps)
+        self.main_sizer.Add(lbl_all_deps, 0, wx.EXPAND)
+        self.main_sizer.Add(self.dep_list_lbo, 3, wx.EXPAND)
+        self.main_sizer.Add(add_dep_btn, 1, wx.ALL | wx.ALIGN_CENTER)
+        self.main_sizer.Add(refresh_lbl, 0, wx.ALL | wx.ALIGN_CENTER)
+        self.main_sizer.Add(refresh_btn, 0, wx.ALL | wx.ALIGN_CENTER)
+
+
+        self.main_sizer.AddSpacer(30)
+
+        self.SetSizer(self.main_sizer)
+
+    def refresh_deps(self, event):
+        with open(self.cfg_file, "r") as config:
+            self.data = json.load(config)
+            choices = self.data["departments"]
+            self.dep_list_lbo.Set(choices)
+
+    def on_search(self, event):
+        search_term = self.search.GetValue()
+        filtered = [x for x in self.choices if search_term.lower() in x.lower()]
+
+        self.update_search_results(filtered)
+        return filtered
+
+    def update_search_results(self, filtered):
+        self.search_results_lbo.Clear()
+        self.search_results_lbo.Append(filtered)
+
+    def add_dep(self, event):
+        add_dep_dlg = AddDepDlg()
+        add_dep_dlg.ShowModal()
+
+
+class AddDepDlg(wx.Dialog):
+    def __init__(self):
+        wx.Dialog.__init__(self, None, wx.ID_ANY, 'Bölüm / Ders Ekle', size=(200, 200))
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.cfg_file = "settings.json"
+        txt = 'Bölüm / ders ekleyin:'
+        label = wx.StaticText(self, label=txt)
+        self.main_sizer.Add(label, 1, wx.EXPAND, 5)
+        self.dep_txt = wx.TextCtrl(self, name="new_dep_name")
+        self.dep_btn = wx.Button(self, wx.ID_ANY, label="Kaydet")
+        self.dep_btn.Bind(wx.EVT_BUTTON, self.add_new)
+        self.main_sizer.Add(self.dep_txt, 1, wx.EXPAND, 3)
+        self.main_sizer.AddSpacer(30)
+        self.main_sizer.Add(self.dep_btn, 1, wx.ALL | wx.ALIGN_CENTER)
+        with open(self.cfg_file, "r") as config:
+            self.data = json.load(config)
+            self.num_col = self.data["num_col"]
+            self.grade_col = self.data["grade_col"]
+            self.departments = self.data["departments"]
+        self.SetSizer(self.main_sizer)
+
+    def add_new(self, event):
+        value = self.dep_txt.GetValue()
+
+        with open(self.cfg_file, "w") as configw:
+            self.departments.append(value)
+            all_data = {"num_col": self.num_col, "grade_col": self.grade_col, "departments": self.departments}
+
+            json.dump(all_data, configw, indent=4, ensure_ascii=False)
+
+        wx.Window.Close(self)
+
+
 if __name__ == "__main__":
     app = wx.App(False)
+    current_dir = os.getcwd()
     frame = MyFrame()
     app.MainLoop()
--- alms2obsWx/funcs.py
+++ alms2obsWx/funcs.py
@@ -1,11 +1,22 @@
 import os
 import openpyxl
 import pandas as pd
+import json
+
+from zipfile import BadZipfile
 from deps_list import deps_list
 
 
+
 # Mods klasörünü oluştur ve düzenlenmiş dosyaları buraya kaydet
-def organize_results(folder_path, list_of_deps):
+def organize_results(folder_path, list_of_deps, current_dir):
+    cfg_file = "settings.json"
+    with open(cfg_file, "r") as config:
+        data = json.load(config)
+        num_col = data["num_col"]
+        grade_col = data["grade_col"]
+        dep_cell = data["dep_cell"]
+
     os.chdir(folder_path)
     files = os.listdir(folder_path)
 
@@ -16,81 +27,88 @@ def organize_results(folder_path, list_of_deps):
     except FileExistsError:
         pass
     mods_path = folder_path + "/mods/"
+
     idx = 1
-    for f in files:
-        if f.endswith('.xlsx'):
-            file_path = str(folder_path) + "/{}".format(f)
-            wb = openpyxl.load_workbook(file_path)
-            ws = wb.active
-            dep = ws['C2']
-            dep_value = dep.value
-            # C2 hücresinde bölüm ismini bulup dosya isimlerini ayarlamak için:
-            if dep_value:
-                for department in deps_list:
-                    if department in dep_value:
-                        dep_name = department
-                        list_of_deps[idx] = dep_name
-                        idx += 1
-                    
-            
+    try:
+        for f in files:
+            if f.endswith('.xlsx'):
+                file_path = str(folder_path) + "/{}".format(f)
+                wb = openpyxl.load_workbook(file_path)
+                ws = wb.active
+                dep = ws[dep_cell]
+                dep_value = dep.value
 
-                ws.delete_cols(2, 2)  # B:C arası gereksizleri sil
-                ws.delete_cols(3, 6)  # J sil
-                ws.delete_rows(1, 1)  # 1.satırı sil
+                # Tercihlerde belirtilen bölüm hücresinde bölüm ismini bulup dosya isimlerini ayarlamak için:
+                if dep_value:
+                    for department in deps_list:
+                        if department in dep_value:
+                            dep_name = department
+                            list_of_deps[idx] = dep_name
+                            idx += 1
 
-                col_A = []
-                col_B = []
+                    ws.delete_rows(1, 1)  # 1.satırı sil
 
-                ws.column_dimensions['A'].width = 20  # A sütun genişiliğini azaltma
-                for col_cell in ws['A']:  # A sütunundaki hücreleri number formatına getirir.
-                    try:
-                        col_cell.number_format = '0'
-                        col_cell.value = int(col_cell.value)
-                        col_A.append(col_cell.value)
-                    except ValueError:  # Bu kısma öğrenci numarası ALMS'de görünmeyen öğrenciler için bir uyarı eklenecek.
-                        pass
+                    col_A = []
+                    col_B = []
 
-                for col_cell in ws['B']:
-                    if col_cell.value != None:
+                    ws.column_dimensions[num_col].width = 20  # A sütun genişliğini azaltma
+                    for col_cell in ws[num_col]:  # A sütunundaki hücreleri number formatına getirir.
                         try:
-                            col_cell.number_format = '0'  # B sütunundaki hücreleri number formatına getirir.
-                            col_cell.value = int(
-                                float(col_cell.value.replace(",", ".")))  # Sayı decimal işaretini ,'den .'ya çevirir.
-                        except AttributeError:
+                            col_cell.number_format = '0'
+                            col_cell.value = int(col_cell.value)
+                            col_A.append(col_cell.value)
+                        except ValueError:  # Bu kısma öğrenci numarası ALMS'de görünmeyen öğrenciler için bir uyarı eklenecek.
+                            import sys, traceback
+                            xc = traceback.format_exception(*sys.exc_info())
+
+                    for col_cell in ws[grade_col]:
+                        if col_cell.value is not None:
+                            try:
+                                col_cell.number_format = '0'  # B sütunundaki hücreleri number formatına getirir.
+                                col_cell.value = int(
+                                    float(col_cell.value.replace(",", ".")))  # Sayı decimal işaretini ,'den .'ya çevirir.
+                            except AttributeError:
+                                pass
+                        else:
+                            pass
+                        col_B.append(col_cell.value)
+
+                    all_data = zip(col_A, col_B)  # col_A ve col_B'de topladıklarımızı eşleştir.
+                    all_data_list = list(all_data)  # üsttekini listeye çevir
+                    sorted_list = sorted(all_data_list, key=lambda x: x[1], reverse=True)  # Notları büyükten küçüğe sıralar
+                    df = pd.DataFrame(sorted_list)  # Sıralanmış halini pandas dataframe'e çevirir
+
+                    if "(İÖ)" not in dep_value:
+                        try:
+                            writer = pd.ExcelWriter("mods/day/{}.xlsx".format(dep_name), engine='xlsxwriter')
+                            df.to_excel(writer, index=False, sheet_name='Sheet1')
+                            wb_alldeps = writer.book
+                            ws_alldeps = writer.sheets['Sheet1']
+                            fmt = wb_alldeps.add_format({'num_format': '0'})  # Sayı formatını belirle
+                            ws_alldeps.set_column('A:B', 20, fmt)  # Gerekli sütunları seç ve sayı formatını uygula
+                            writer.save()
+                        except FileNotFoundError:
                             pass
                     else:
-                        pass
-                    col_B.append(col_cell.value)
+                        try:
+                            dep_name = dep_name + " (İÖ)"
+                            writer = pd.ExcelWriter("mods/night/{}.xlsx".format(dep_name), engine='xlsxwriter')
+                            df.to_excel(writer, index=False, sheet_name='Sheet1')
+                            wb_alldeps = writer.book
+                            ws_alldeps = writer.sheets['Sheet1']
+                            fmt = wb_alldeps.add_format({'num_format': '0'})  # Sayı formatını belirle
+                            ws_alldeps.set_column('A:B', 20, fmt)  # Gerekli sütunları seç ve sayı formatını uygula
+                            writer.save()
+                        except FileNotFoundError:
+                            pass
+    except BadZipfile:
+        pass
 
-                all_data = zip(col_A, col_B)  # col_A ve col_B'de topladıklarımızı eşleştir.
-                all_data_list = list(all_data)  # üsttekini listeye çevir
-                sorted_list = sorted(all_data_list, key=lambda x: x[1], reverse=True)  # Notları büyükten küçüğe sıralar
-                df = pd.DataFrame(sorted_list)  # Sıralanmış halini pandas dataframe'e çevirir
-
-                if "(İÖ)" not in dep_value:
-                    try:
-                        writer = pd.ExcelWriter("mods/day/{}.xlsx".format(dep_name), engine='xlsxwriter')
-                        df.to_excel(writer, index=False, sheet_name='Sheet1')
-                        wb_alldeps = writer.book
-                        ws_alldeps = writer.sheets['Sheet1']
-                        fmt = wb_alldeps.add_format({'num_format': '0'})  # Sayı formatını belirle
-                        ws_alldeps.set_column('A:B', 20, fmt)  # Gerekli sütunları seç ve sayı formatını uygula
-                        writer.save()
-                    except FileNotFoundError:
-                        pass
-                else:
-                    try:
-                        dep_name = dep_name + " (İÖ)"
-                        writer = pd.ExcelWriter("mods/night/{}.xlsx".format(dep_name), engine='xlsxwriter')
-                        df.to_excel(writer, index=False, sheet_name='Sheet1')
-                        wb_alldeps = writer.book
-                        ws_alldeps = writer.sheets['Sheet1']
-                        fmt = wb_alldeps.add_format({'num_format': '0'})  # Sayı formatını belirle
-                        ws_alldeps.set_column('A:B', 20, fmt)  # Gerekli sütunları seç ve sayı formatını uygula
-                        writer.save()
-                    except FileNotFoundError:
-                        pass
-    return mods_path
+    try:
+        if xc:
+            return xc
+    except UnboundLocalError:
+        return mods_path
 
 
 # Mods klasöründeki gerekli klasörü aç ve panoya kopyala
@@ -99,7 +117,7 @@ def create_data(folder_path, id):
     chosen_file = folder_path + id + ".xlsx"
     wb = openpyxl.load_workbook(chosen_file)
     ws = wb.active
-    copied_cells = ws['A2:B200']
+    copied_cells = ws['A2:B5000']
     for a, b in copied_cells:
         if a.value and b.value != -1:
             data.append([a.value, b.value])
